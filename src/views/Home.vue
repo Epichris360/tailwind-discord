9d3efbd28c3e9b79276f42e348c499768fc8b888
cristian <segoviacristian23@hotmail.com>
2020-06-20 20:40:45 -0400

    initial layout done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex">
+    <div class="w-20 flex-none flex flex-col min-h-screen h-screen">
+      <div class="overflow-y-auto" >
+        logos Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, quaerat quibusdam, amet veritatis blanditiis adipisci laborum totam delectus aperiam quo, recusandae labore nihil sequi nisi et. Esse ut iusto ab, minima vel quasi fugit eius perspiciatis. Eius aliquam minima numquam repellat a corrupti? Nemo obcaecati deserunt, ex facilis quam quis possimus ratione aliquid reiciendis autem nulla commodi, placeat quibusdam voluptatem officiis voluptatibus accusamus, mollitia animi rem quos iste. Minima pariatur praesentium aut nam, repellat quas deleniti sed veniam, molestias modi animi laboriosam laudantium quos optio rem repudiandae dolor sunt labore, blanditiis quia nobis! Ratione quidem tempore totam, blanditiis, quia harum delectus aliquid tenetur dolorem voluptates rerum enim consequuntur tempora nihil quod accusamus. Doloremque, repellendus! Iure, quae aperiam! Asperiores quidem quia soluta excepturi culpa consequatur commodi ex? Mollitia deleniti et enim sed ullam amet magnam sint quasi? Id fugit dolores iusto non nobis, odit cupiditate voluptate velit! Reprehenderit odit maxime voluptate velit ipsa? Esse aperiam dolore, repudiandae, suscipit tempora exercitationem optio ad alias odit praesentium nostrum. At corrupti temporibus atque labore commodi aspernatur asperiores esse facere, officiis incidunt expedita cumque similique rem aperiam quos accusantium sapiente rerum repellendus ullam est consequuntur voluptas dignissimos. Quibusdam quidem deleniti quasi autem aliquid voluptatum enim!
+      </div>
+    </div>
+    <div class="flex flex-col min-h-screen h-screen w-screen" >
+      <div class="bg-red-500 flex" >
+        <div class="bg-red-300 w-56 flex-none">Tailwind Css</div>
+        <div>Rest of top bar</div>
+      </div>
+      <div class="flex-1 bg-green-600 flex overflow-y-hidden" >
+        <div class="w-56 flex-none bg-green-300 flex flex-col justify-between">
+          <div class="overflow-y-auto" >Hashtag sidebar</div>
+          <div>buttons</div>
+        </div>
+        <div class="bg-green-800 flex-1 flex justify-between">
+          <div class="bg-indigo-300 flex-1 flex flex-col justify-between" >
+            <div class="overflow-y-auto">Display Chat Context..</div>
+            <div class="bg-indigo-200" >Write message here</div>
+          </div>
+          <div class="bg-indigo-600 w-56 flex-none overflow-y-auto" >Users sidebar..</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
